1df52fb6a6b450904fbb77aa16d15a5757f6eb50
Thiago da Silva Adriano <tadriano.net@gmail.com>
2019-12-23 06:31:42 -0300

    first commit

---
+++ Api/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ValuesController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+            
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var client = _clientFactory.CreateClient();
+            client.BaseAddress = new Uri("https://pubvideos.band.uol.com.br/Programa/Grade");
+            string response = await client.GetStringAsync(client.BaseAddress);
+            var result = JsonConvert.DeserializeObject(response);
+
+            return Ok(result);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
